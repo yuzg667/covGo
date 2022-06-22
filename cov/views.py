6d7a15f364b8e471bd6259fa2a5e4c9db3de6b26
yuzg667 <yuzg667@126.com>
2022-06-22 19:11:10 +0800

    代码优化，初始化执行时初始化目录

--- cov/views.py
+++ cov/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+# django启动时执行
+from utils.execCmd import execCmd
+from django.conf import settings
+def initFileAndDir():
+    execCmd(f'''mkdir -p {settings.BASE_DIR}/../covFilesDir''')
+    execCmd(f'''chmod 777 {settings.BASE_DIR}/../covFilesDir/*''')
+    execCmd(f'''chmod 777 {settings.BASE_DIR}/cmdTools''')
+    execCmd(f'''chmod 777 {settings.BASE_DIR}/cmdTools/*''')
+initFileAndDir()
