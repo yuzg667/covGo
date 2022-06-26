554e7fc3fbf17b0b9eff84cea60333ac49d36483
yuzg667 <yuzg667@126.com>
2022-06-26 15:04:50 +0800

    空xml更名为error的不过修改；生成html无覆盖率时数值置0

--- utils/covUtils.py
+++ utils/covUtils.py
@@ -17,22 +17,27 @@ def getClientServerList():
     print(clientServerList)
     return clientServerList
 
+
 # 定义覆盖率文件存储位置
-def covReportsPath(gitProjectName,covTaskId):
-    covReportsPath = os.path.join(settings.BASE_DIR, '..', 'covFilesDir', 'covReports',str(gitProjectName).strip(), str(covTaskId).strip())
+def covReportsPath(gitProjectName, covTaskId):
+    covReportsPath = os.path.join(settings.BASE_DIR, '..', 'covFilesDir', 'covReports', str(gitProjectName).strip(),
+                                  str(covTaskId).strip())
     execCmd(f'''chmod 777 {covReportsPath}''')
     return covReportsPath
 
+
 # 生成runId
-def generateRunId(t, covTaskId,originalHostPort):
+def generateRunId(t, covTaskId, originalHostPort):
     patternHostPort = re.compile(r'''\/\/(.*)''')
     if originalHostPort:
-        hostPort = re.findall(patternHostPort,str(originalHostPort))[0]
-        hostPort = hostPort.replace(':','_')
+        hostPort = re.findall(patternHostPort, str(originalHostPort))[0]
+        hostPort = hostPort.replace(':', '_')
         res = f'''{t}-{covTaskId}-{hostPort}'''
     else:
         res = f'''{t}-{covTaskId}'''
     return res
+
+
 # hostPort = 'http://192.168.56.101:46599'
 # generateRunId(1,hostPort)
 
@@ -52,7 +57,12 @@ def crawlCovFromHtml(htmlPath):
     patternCoverage = re.compile(r'''<li><b>Coverage</b>: (.*)</li>''')
     coverage = re.findall(patternCoverage, htmlStr)[0]
 
-    return {"diffLineTotal":diffLineTotal, "missLineTotal":missLineTotal, "coverage":coverage}
+    # 如果生成的html有这句话，说明选择的两个分支无差异
+    if "No lines with coverage information in this diff." in htmlStr:
+        diffLineTotal = 0
+        missLineTotal = 0
+        coverage = 0
+    return {"diffLineTotal": diffLineTotal, "missLineTotal": missLineTotal, "coverage": coverage}
 
 # path = '''D://work_space//covFilesDir//covReports//simple-go-server//1//merge20220622193826355853-1.html'''
 # res = crawlCovFromHtml(path)
