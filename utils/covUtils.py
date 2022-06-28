15b560370e9c79b07200dea539f5334c59115bd6
yuzg667 <yuzg667@126.com>
2022-06-28 09:45:40 +0800

    实现全量覆盖率爬取；全量覆盖率bug修改；初始化init.sql更新

--- utils/covUtils.py
+++ utils/covUtils.py
@@ -47,21 +47,37 @@ def crawlCovFromHtml(htmlPath):
     s = BeautifulSoup(htmlhandle, 'lxml')
 
     htmlStr = str(s)  # 正则必须是string
+    # 取文件名判断是全量覆盖率 还是 增量覆盖率
+    htmlFileName = htmlPath.split('/')[-1]
+    # 增量覆盖率
+    if htmlFileName.startswith('merge'):
+        # 如果生成的html有这句话，说明选择的两个分支无差异
+        if "No lines with coverage information in this diff." in htmlStr:
+            diffLineTotal = 0
+            missLineTotal = 0
+            coverage = 0
+        elif "No lines with coverage information in this diff." not in htmlStr:
+            patternDiffLineTotal = re.compile(r'''<li><b>Total</b>: (.*) lines</li>''')
+            diffLineTotal = re.findall(patternDiffLineTotal, htmlStr)[0]
 
-    # 如果生成的html有这句话，说明选择的两个分支无差异
-    if "No lines with coverage information in this diff." in htmlStr:
-        diffLineTotal = 0
-        missLineTotal = 0
-        coverage = 0
-    elif "No lines with coverage information in this diff." not in htmlStr:
-        patternDiffLineTotal = re.compile(r'''<li><b>Total</b>: (.*) lines</li>''')
-        diffLineTotal = re.findall(patternDiffLineTotal, htmlStr)[0]
+            patternMissLineTotal = re.compile(r'''<li><b>Missing</b>: (.*) lines</li>''')
+            missLineTotal = re.findall(patternMissLineTotal, htmlStr)[0]
 
-        patternMissLineTotal = re.compile(r'''<li><b>Missing</b>: (.*) lines</li>''')
-        missLineTotal = re.findall(patternMissLineTotal, htmlStr)[0]
+            patternCoverage = re.compile(r'''<li><b>Coverage</b>: (.*)</li>''')
+            coverage = re.findall(patternCoverage, htmlStr)[0]
+    # 全量覆盖率
+    elif htmlFileName.startswith('full'):
+        patternTargetHtml = re.compile(r'''<td class="percent">(.*)</td>''')
+        targetHtml = re.findall(patternTargetHtml, htmlStr)[-1]
 
-        patternCoverage = re.compile(r'''<li><b>Coverage</b>: (.*)</li>''')
-        coverage = re.findall(patternCoverage, htmlStr)[0]
+        patternCoverage = re.compile(r'''<code>(.*)</code></td>''')
+        coverage = re.findall(patternCoverage, targetHtml)[-1]
+
+        patternLineTotal = re.compile(r'''<td class="linecount"><code>(.*)</code>''')
+        lineTotal = re.findall(patternLineTotal, targetHtml)[-1]
+        diffLineTotal = lineTotal  # 实际为总行数
+        diffLineTotal = diffLineTotal.split('/')[1]
+        missLineTotal = int(lineTotal.split('/')[1]) - int(lineTotal.split('/')[0])
     return {"diffLineTotal": diffLineTotal, "missLineTotal": missLineTotal, "coverage": coverage}
 
 # path = '''D://work_space//covFilesDir//covReports//simple-go-server//1//merge20220622193826355853-1.html'''
